411c8415082c552cc9bbb30ca58c9f21d0f39ee3
Pavan Mistry <pavan32100@gmail.com>
2025-10-28 14:51:17 -0400

    Updated Skills layout

--- Portfolio-frontend/src/components/Skills/Skills.jsx
+++ Portfolio-frontend/src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Particle from "../Particle";
 
 import { Container, Col, Row } from "react-bootstrap";
@@ -6,21 +6,21 @@ import { Container, Col, Row } from "react-bootstrap";
 function Skills() {
   const [skills, setSkills] = useState([]);
 
-  useEffect(() => {
-    const fetchSkills = async () => {
-      try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/skills`);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        const sortedSkills = data.sort((a, b) => a.skillName.localeCompare(b.skillName));
-        setSkills(sortedSkills);
-      } catch (error) {
-        console.error("Error fetching skills:", error);
+  const fetchSkills = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/skills`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-    };
+      const data = await response.json();
+      const sortedSkills = data.sort((a, b) => a.skillName.localeCompare(b.skillName));
+      setSkills(sortedSkills);
+    } catch (error) {
+      console.error("Error fetching skills:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchSkills();
   }, []);
 
@@ -31,14 +31,16 @@ function Skills() {
         <h1 className="display-4 pb-3">
           Professional <strong className="txt-color">Skills</strong>
         </h1>
-        <Row className="justify-content-center align-items-end">
+        <Row className="justify-content-evenly align-items-start">
           {skills.map((skill, index) => (
             <Col key={index} xs={4} md={2} className="mb-3 skill-width">
-              <img
-                src={skill.skillImage}
-                alt={`Logo of ${skill.skillName}`}
-                className="img-fluid rounded rounded-3 mb-1 skill-img"
-              />
+              <div className="skill-img">
+                <img
+                  src={skill.skillImage}
+                  alt={`Logo of ${skill.skillName}`}
+                  className="img-fluid rounded rounded-3 mb-1"
+                />
+              </div>
               <p>{skill.skillName}</p>
             </Col>
           ))}
